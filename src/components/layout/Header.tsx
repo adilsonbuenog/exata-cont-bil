46a28369b2415d8284556a51b0b30259baf77305
Luciano Giraldi <101355653+LucianoGiraldi@users.noreply.github.com>
2025-12-16 12:08:41 -0300

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
           <img
             src="/logo-exata.jpeg"
             alt="Exata Assessoria Contábil"
-            className="h-12 sm:h-14 w-auto object-contain"
+            className="h-20 sm:h-24 w-auto object-contain"
           />
         </Link>
 
