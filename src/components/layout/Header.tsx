7dc3ab36d359c271e6e25ae6d9d9e7ed76f4da45
Luciano Giraldi <101355653+LucianoGiraldi@users.noreply.github.com>
2025-12-16 15:44:35 -0300

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
           {/* Logo */}
         <Link to="/" className="flex items-center gap-3">
           <img
-            src="/logo-exata2.jpeg"
+            src="/logo-exata2.png"
             alt="Exata Assessoria Contábil"
             className="h-16 sm:h-18 w-auto object-contain"
           />
